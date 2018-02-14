7ad7bff7bea7a4c1709b951b7cfe5ac6fbd48027
Lorenzo Pincinato <lorenzopincinato@gmail.com>
2018-02-14 21:35:44 -0200

    Added message, date and approved attributes to Spot

--- src/main/java/xyz/lorenzopincinato/spotted/cotuca/api/models/spot/Spot.java
+++ src/main/java/xyz/lorenzopincinato/spotted/cotuca/api/models/spot/Spot.java
@@ -3,11 +3,22 @@ package xyz.lorenzopincinato.spotted.cotuca.api.models.spot;
 import io.yawp.repository.IdRef;
 import io.yawp.repository.annotations.Endpoint;
 import io.yawp.repository.annotations.Id;
+import io.yawp.repository.annotations.Text;
+import lombok.Data;
 
+import java.util.Date;
+
+@Data
 @Endpoint(path = "/spots")
 public class Spot {
 
     @Id
-    IdRef<Spot> id;
+    private IdRef<Spot> id;
 
+    @Text
+    private String message;
+
+    private Date date;
+
+    private boolean approved;
 }
